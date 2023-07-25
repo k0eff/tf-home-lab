fa1c891c27993d383cbb697abff3406bcc0df38c
Krasimir Koeff <kr@koeff.com>
2023-07-25 22:35:25 +0300

    fixes: create vm

--- terraspace/app/modules/vmRes/outputs.tf
+++ terraspace/app/modules/vmRes/outputs.tf
@@ -3,5 +3,5 @@ output "vm_ids" {
 }
 
 output "vm_ip_addresses" {
-  value = [for vm in vsphere_virtual_machine.vm : vm.network_interface.0.ipv4_address]
+  value = [for vm in vsphere_virtual_machine.vm : vm.default_ip_address]
 }
